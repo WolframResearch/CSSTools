6a372fbcbb52cc6f33bebf556cd6b0c435ebe10a
Kevin Daily <kevind@wolfram.com>
2019-03-15 08:40:42 -0500

    Project follows documented paclet structure.
    The main functions have all been exposed.
    CSS interpretations can now be resolved for specific boxes e.g. PaneBox.
    I'm still not happy about the exposed function names.

---
+++ build/CSSTools/PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2019.03.05*)
+
+Paclet[
+  Name -> "CSSTools",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
