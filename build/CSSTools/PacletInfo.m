70cde39e8a7bb6f35f88831548d4b708c9bfc69d
Kevin Daily <kevind@wolfram.com>
2020-07-16 13:20:08 -0700

    Rebuild documentation in 12.1.1:
    The rebuild is motivated by renaming CSSTarget(s) to CSSSubject(s). But
    there are nice fixes to Dataset in 12.1.1 so less of the content should
    be occluded as well.

--- build/CSSTools/PacletInfo.m
+++ build/CSSTools/PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2020/03/12*)
+(* created 2020/07/16*)
 
 Paclet[
     Name -> "CSSTools",
-    Version -> "1.2.1",
+    Version -> "1.3.1",
     MathematicaVersion -> "11+",
     Description -> "Import and process CSS into Wolfram Language options.",
     Creator -> "Kevin Daily",
